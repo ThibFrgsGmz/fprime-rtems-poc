b6a3d0ea646cdcc027f8963bb23826ff4e7dc679
ThibFrgsGmz <farges.thibault@gmail.com>
2023-02-01 22:18:49 +0100

    chore(cmake): add PlatformType to be compiled

--- fprime-rtems/cmake/platform/RTEMS.cmake
+++ fprime-rtems/cmake/platform/RTEMS.cmake
@@ -6,5 +6,5 @@
 add_definitions(-DTGT_OS_TYPE_RTEMS)
 set(TGT_OS_TYPE_RTEMS ON)
 
-# include(${CMAKE_CURRENT_LIST_DIR}/Support/${CMAKE_SYSTEM_NAME}/${TGT_HW_TYPE}.cmake)
+include("${CMAKE_CURRENT_LIST_DIR}/Support/${CMAKE_SYSTEM_PROCESSOR}.cmake")
 
---
+++ fprime-rtems/cmake/platform/Support/leon3.cmake
@@ -0,0 +1,3 @@
+
+
+include_directories("${FPRIME_FRAMEWORK_PATH}/../fprime-leon3/Types")
